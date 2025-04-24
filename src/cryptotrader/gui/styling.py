c74261647b2a74b5a4672c8762fdf5bab0bd6cf3
Patrick Reid <patrickjreid@gmail.com>
2025-04-24 13:38:04 -0600

    I am a bit blocked

--- src/cryptotrader/gui/styling.py
+++
@@ -1,7 +0,0 @@
-# src/cryptotrader/gui/styling.py
-BG_COLOR = "gray12"
-BG_COLOR_2 = "#1c2c5c"
-FG_COLOR = "white"
-FG_COLOR_2 = "SteelBlue1"
-GLOBAL_FONT = ("Calibri", 11, "normal")
-BOLD_FONT = ("Calibri", 11, "bold")
